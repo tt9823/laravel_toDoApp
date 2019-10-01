9111781fd1918f2e36bcee5ae651c6117cc56f02
鳥塚武 <>
2019-10-01 21:38:26 +0900

    フォルダ、タスク一覧リスト

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+    {
+        $folders = Folder::all();
+        $current_folder = Folder::find($id);
+        // $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
+
+        return view('tasks./index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
+}
