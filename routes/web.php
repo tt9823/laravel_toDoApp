aff105a665d73a9ec216335817222da2b81091d2
鳥塚武 <>
2019-11-03 12:46:14 +0900

    signin/login機能

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,3 +24,5 @@ Route::get('folders/{id}/tasks/create', 'TaskController@showCreateForm') -> name
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm') -> name('tasks.edit');
 Route::post('folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Route::get('/', 'HomeController@index')->name('home');
+Auth::routes();
